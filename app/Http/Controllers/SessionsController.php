10c46a2625bf1865fe0ae4c7afb46c6a2215b89a
Tamar Sinauridze <sin165165@gmail.com>
2024-02-28 18:47:31 +0400

    refactor: change login validation code

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -14,8 +14,7 @@ class SessionsController extends Controller
     {
         if(!auth()->attempt($request->validated())) {
             throw ValidationException::withMessages([
-                'email' => __('auth.failed'),
-                'password' => __('auth.failed'),
+                'password' => __('auth.password'),
             ]);
         }
         return redirect()->route('home');
--- app/Http/Requests/StoreSessionsRequest.php
+++ app/Http/Requests/StoreSessionsRequest.php
@@ -6,15 +6,10 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreSessionsRequest extends FormRequest
 {
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            'email' => 'required|email',
+            'email' => 'required|email|exists:users,email',
             'password' => 'required|min:4',
         ];
     }
