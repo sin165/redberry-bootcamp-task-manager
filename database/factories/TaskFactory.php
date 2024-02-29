03c7ce0886c45e4b10b69cf924393090176e466b
Tamar Sinauridze <sin165165@gmail.com>
2024-02-29 16:12:20 +0400

    feat: create database seeder

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
@@ -17,7 +18,16 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'name' => [
+                'en' => fake()->sentence(),
+                'ka' => 'ლორემ იპსუმ უნიფორმა ფიჭვის იწყო, სოფიოც ართლაცდა.',
+            ],
+            'description' => [
+                'en' => fake()->sentence(20),
+                'ka' => 'ლორემ იპსუმ დაგიბრუნებ უყრისო შევიწროვებული შეფის მისგან ყვირილს ლოკოკინებისა. ფეხთით ინციდენტი მაგას დამმშვიდდი კრიტიკის ვიღა ილაპარაკებდი ასეთს.',
+            ],
+            'due_date' => fake()->dateTimeBetween('-5days', '+10 days'),
         ];
     }
 }
