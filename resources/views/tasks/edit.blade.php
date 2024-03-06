80d429984b6bf372543cba8ba3b4927e88f94308
Tamar Sinauridze <sin165165@gmail.com>
2024-03-07 00:25:40 +0400

    refactor: extract task fields into a component

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -9,13 +9,8 @@
                 {{ __('tasks.edit_task') }}
             </h1>
             <form class="flex flex-col gap-6" method="POST" action="{{ route('tasks.update', $task->id) }}">
-                @csrf
                 @method('PATCH')
-                <x-form.input name="name[en]" label="{{ __('forms.name_en') }}" placeholder="{{ __('forms.name_en_placeholder') }}" required existingData="{{ $task->getTranslation('name', 'en') }}" />
-                <x-form.input name="name[ka]" label="{{ __('forms.name_ka') }}" placeholder="{{ __('forms.name_ka_placeholder') }}" required existingData="{{ $task->getTranslation('name', 'ka') }}" />
-                <x-form.textarea name="description[en]" label="{{ __('forms.description_en') }}" placeholder="{{ __('forms.description_en_placeholder') }}" required existingData="{{ $task->getTranslation('description', 'en') }}" />
-                <x-form.textarea name="description[ka]" label="{{ __('forms.description_ka') }}" placeholder="{{ __('forms.description_ka_placeholder') }}" required existingData="{{ $task->getTranslation('description', 'ka') }}" />
-                <x-form.date-input name="due_date" label="{{ __('forms.due_date') }}" required  existingData="{{ $task->due_date->format('Y-m-d') }}" />
+                <x-form.task-fields :task="$task" />
                 <x-form.button>{{ __('tasks.edit_changes') }}</x-form.button>
             </form>
         </div>
