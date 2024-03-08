df485c29bf0961cce3929c2a8935b5fe49b8d46e
Tamar Sinauridze <sin165165@gmail.com>
2024-03-09 00:31:55 +0400

    feat: translate error messages

--- lang/ka/validation.php
+++ lang/ka/validation.php
@@ -1,6 +1,21 @@
 <?php
 
 return [
-	'english'  => 'მხოლოდ ლათინური სიმბოლოები',
-	'georgian' => 'მხოლოდ ქართული სიმბოლოები',
+	'english'          => 'მხოლოდ ლათინური სიმბოლოები',
+	'georgian'         => 'მხოლოდ ქართული სიმბოლოები',
+	'min'              => ['string'  => 'მინიმუმ :min სიმბოლო.'],
+	'exists'           => 'მითითებული :attribute არ მოიძებნა.',
+	'current_password' => 'პაროლი არასწორია.',
+	'required_with'    => 'ველი :attribute სავალდებულოა, როცა :values შევსებულია.',
+	'same'             => 'ველი :attribute უნდა ემთხვეოდეს ველს :other.',
+	'image'            => ':attribute უნდა იყოს სურათი.',
+
+	'attributes' => [
+		'email'            => 'მეილი',
+		'current_password' => '"მიმდინარე პაროლი"',
+		'new_password'     => '"ახალი პაროლი"',
+		'retype_password'  => '"გაიმეორეთ პაროლი"',
+		'profile_picture'  => 'პროფილის ფოტო',
+		'cover'            => 'ქავერი',
+	],
 ];
