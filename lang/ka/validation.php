c82d65c532d3c1d6cdc9df8aab0b49bcf9d73543
Tamar Sinauridze <sin165165@gmail.com>
2024-03-12 14:55:21 +0400

    fix: translate email validation text

--- lang/ka/validation.php
+++ lang/ka/validation.php
@@ -1,14 +1,15 @@
 <?php
 
 return [
-	'english'          => 'მხოლოდ ლათინური სიმბოლოები',
-	'georgian'         => 'მხოლოდ ქართული სიმბოლოები',
-	'min'              => ['string'  => 'მინიმუმ :min სიმბოლო.'],
-	'exists'           => 'მითითებული :attribute არ მოიძებნა.',
-	'current_password' => 'პაროლი არასწორია.',
-	'required_with'    => 'ველი :attribute სავალდებულოა, როცა :values შევსებულია.',
-	'same'             => 'ველი :attribute უნდა ემთხვეოდეს ველს :other.',
-	'image'            => ':attribute უნდა იყოს სურათი.',
+	'english'           => 'მხოლოდ ლათინური სიმბოლოები',
+	'georgian'          => 'მხოლოდ ქართული სიმბოლოები',
+	'min'               => ['string'  => 'მინიმუმ :min სიმბოლო.'],
+	'exists'            => 'მითითებული :attribute არ მოიძებნა.',
+	'current_password'  => 'პაროლი არასწორია.',
+	'required_with'     => 'ველი :attribute სავალდებულოა, როცა :values შევსებულია.',
+	'same'              => 'ველი :attribute უნდა ემთხვეოდეს ველს :other.',
+	'image'             => ':attribute უნდა იყოს სურათი.',
+	'email'             => ':attribute უნდა იყოს ვალიდური ელ-ფოსტის მისამართი.',
 
 	'attributes' => [
 		'email'            => 'მეილი',
